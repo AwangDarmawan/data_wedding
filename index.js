f874751dbbfc9eb175aa2388408a7b2e30946956
AwangDarmawan <adarmawan468@gmail.com>
2025-09-24 10:11:34 +0700

    bismilah

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
+
+
 // const jsonServer = require("json-server"); 
 // const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
+// const router = jsonServer.router("db.json"); // <-- menunjuk ke file db.json
 // const middlewares = jsonServer.defaults();
 // const port = process.env.PORT || 3001; 
 
@@ -9,19 +12,25 @@
 
 // server.listen(port, () => {
 //     console.log(`JSON Server berjalan di port ${port}`);
-//   });
+// });
+const jsonServer = require("json-server");
+const cors = require("cors");
 
-
-
-const jsonServer = require("json-server"); 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // <-- menunjuk ke file db.json
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; 
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-server.listen(port, () => {
-    console.log(`JSON Server berjalan di port ${port}`);
-});
+// export untuk Vercel
+module.exports = server;
+
+// kalau running lokal
+if (require.main === module) {
+  const port = process.env.PORT || 3001;
+  server.listen(port, () => {
+    console.log(`JSON Server berjalan di http://localhost:${port}`);
+  });
+}
